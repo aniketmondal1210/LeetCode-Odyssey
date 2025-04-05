2005d20f842fc6c54872a66ef61820a2b407b90b
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-04-05 20:51:21 +0530

    Update max_product_of_3_numbers.py

--- 628. Maximum Product of Three Numbers/max_product_of_3_numbers.py
+++ 628. Maximum Product of Three Numbers/max_product_of_3_numbers.py
@@ -1,4 +1,4 @@
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
         sorted_nums = sorted(nums, reverse=True)
-        return max(sorted_nums[0] * sorted_nums[1] * sorted_nums[2], sorted_nums[0] * sorted_nums[-1] * sorted_nums[-2])
+        return max(sorted_nums[0] * sorted_nums[1] * sorted_nums[2],sorted_nums[0] * sorted_nums[-1] * sorted_nums[-2])
