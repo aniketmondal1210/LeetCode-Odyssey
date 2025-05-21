4a174066fe75b4fc3c48d59b367587e5220bd502
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-05-21 12:02:47 +0530

    Create find_the_duplicate_number.py

---
+++ 287. Find the Duplicate Number/find_the_duplicate_number.py
@@ -0,0 +1,7 @@
+from collections import Counter
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        count = Counter(nums)
+        for key, value in count.items():
+            if value > 1:
+                return key
