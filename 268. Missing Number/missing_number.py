1763749eae318c3a85a9007450ef1644923d6ed7
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-08-12 09:24:04 +0530

    Update missing_number.py

--- 268. Missing Number/missing_number.py
+++ 268. Missing Number/missing_number.py
@@ -7,3 +7,19 @@ class Solution:
                 return i
                 break
         return a+1
+
+
+
+OR
+
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        xor_nums = 0
+        xor_all = 0
+        for i in nums:
+            xor_nums ^= i
+        for j in range(len(nums)+1):
+            xor_all ^= j
+        return xor_nums ^ xor_all
