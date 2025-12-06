0a5e26d7d5cf68dcb57717fc9ee4124e812a4dc1
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-12-06 19:25:32 +0530

    Implement getFinalState method for array manipulation

---
+++ 3264. Final Array State After K Multiplication Operations I/final_array_state_after_k_multiplication_operations_i.py
@@ -0,0 +1,6 @@
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        for i in range(k):
+            min_index = nums.index(min(nums))
+            nums[min_index] = min(nums) * multiplier
+        return nums
