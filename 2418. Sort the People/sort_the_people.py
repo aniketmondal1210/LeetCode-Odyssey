cdb414eebd5265fa7af3d3555ea15bcd40a47b24
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-02-05 21:13:37 +0530

    Refactor sortPeople method to use zip and lambda

--- 2418. Sort the People/sort_the_people.py
+++ 2418. Sort the People/sort_the_people.py
@@ -1,6 +1,4 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        people_dict = dict(zip(heights, names))
-        sorted_heights = sorted(people_dict.keys(), reverse=True)
-        result = [people_dict[height] for height in sorted_heights]
-        return result
+        a = sorted(zip(names, heights), key=lambda x: x[1], reverse=True)
+        return [name for name, height in a]
