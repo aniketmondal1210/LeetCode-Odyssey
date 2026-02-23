0017b15f362e333a025ff5fad87ea5ef0335c7a4
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-02-23 09:45:44 +0530

    Create circular_sentence.py

---
+++ 2490. Circular Sentence/circular_sentence.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        a = sentence.split()
+        for i in range(len(a) - 1):
+            if a[i][-1] != a[i+1][0]:
+                return False
+        return a[-1][-1] == a[0][0]
