f2d1a9feccaf3791cccc003b85449febf88af6f7
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-03-02 13:25:25 +0530

    Update trim_trailing_vowels.py

--- 3856. Trim Trailing Vowels/trim_trailing_vowels.py
+++ 3856. Trim Trailing Vowels/trim_trailing_vowels.py
@@ -1,3 +1,15 @@
 class Solution:
     def trimTrailingVowels(self, s: str) -> str:
         return s.rstrip('aeiou')
+
+
+OR
+
+
+class Solution:
+    def trimTrailingVowels(self, s: str) -> str:
+        vowels = "aeiou"
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] not in vowels:
+                return s[:i + 1]
+        return ""
