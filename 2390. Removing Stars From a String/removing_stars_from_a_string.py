ad4efab5e4532b2ea925c5bbec272b83e9a14295
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-04-07 09:56:59 +0530

    Implement star removal from string in Solution class

---
+++ 2390. Removing Stars From a String/removing_stars_from_a_string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        result = []
+        for char in s:
+            if char == '*':
+                result.pop()
+            else:
+                result.append(char)
+        return ''.join(result)
