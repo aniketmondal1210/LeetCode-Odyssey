aa4d14c3ae45ddaf5f2079de4990ea10c91ffdee
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-05-06 19:19:52 +0530

    Implement findMatrix to convert array to 2D array

---
+++ 2610. Convert an Array Into a 2D Array With Conditions/convert_an_array_into_2d_array_with_conditions.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findMatrix(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        count = 0
+        n = len(nums)
+        while count != n:
+            seen = set()
+            row = []
+            for i in range(len(nums)):
+                if nums[i] is not None and nums[i] not in seen:
+                    seen.add(nums[i])
+                    row.append(nums[i])
+                    nums[i] = None
+                    count += 1
+            result.append(row)
+        return result
