e4cdaef73d7ec82a80de6607a352ca33396d32b5
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-06-11 22:09:39 +0530

    Create check_if_object_instance_of_class.js

---
+++ 2618. Check if Object Instance of Class/check_if_object_instance_of_class.js
@@ -0,0 +1,25 @@
+/**
+ * @param {*} obj
+ * @param {*} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function(obj, classFunction) {
+    if (typeof classFunction !== 'function') {
+        return false;
+    }
+
+    if (obj === null || obj === undefined) {
+        return false;
+    }
+
+    let proto = Object.getPrototypeOf(obj);
+
+    while (proto !== null) {
+        if (proto === classFunction.prototype) {
+            return true;
+        }
+        proto = Object.getPrototypeOf(proto);
+    }
+
+    return false;
+};
