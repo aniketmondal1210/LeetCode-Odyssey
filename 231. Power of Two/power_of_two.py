fc31f002b683042673868e7e24eb680ded241a38
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-09-01 10:57:38 +0530

    Simplify isPowerOfTwo method implementation

--- 231. Power of Two/power_of_two.py
+++ 231. Power of Two/power_of_two.py
@@ -1,10 +1,3 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n <= 0:
-            return False
-        else:
-            a = bin(n)[2:]
-            if a.count('1') == 1:
-                return True
-            else:
-                return False
+        return n > 0 and (n & (n - 1)) == 0
