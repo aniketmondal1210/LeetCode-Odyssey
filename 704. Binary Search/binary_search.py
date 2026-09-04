fd9a51c56977efd7c81c63ecae69043ee7defa0e
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-09-04 14:09:50 +0530

    Refactor binary search to iterative approach

--- 704. Binary Search/binary_search.py
+++ 704. Binary Search/binary_search.py
@@ -1,15 +1,13 @@
 class Solution:
-    def BinarySearch(self, arr, l, r, tar):
-        if l > r:
-            return -1
-        m = (l + r) // 2
-        if arr[m] == tar:
-            return m
-        elif arr[m] > tar:
-            return self.BinarySearch(arr, l, m - 1, tar)
-        else:
-            return self.BinarySearch(arr, m + 1, r, tar)
-
     def search(self, nums: List[int], target: int) -> int:
-        start, end = 0, len(nums) - 1
-        return self.BinarySearch(nums, start, end, target)
+        start = 0
+        end = len(nums) - 1
+        while(start <= end):
+            mid = (start + end)//2
+            if nums[mid] == target:
+                return mid
+            elif target < nums[mid]:
+                end = mid - 1
+            elif target > nums[mid]:
+                start = mid + 1
+        return -1
