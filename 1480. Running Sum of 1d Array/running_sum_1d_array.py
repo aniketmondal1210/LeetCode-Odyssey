f7b4b7c1535802679f0aee26ff01bb4936b7fc74
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-09-11 10:35:18 +0530

    Update running_sum_1d_array.py

--- 1480. Running Sum of 1d Array/running_sum_1d_array.py
+++ 1480. Running Sum of 1d Array/running_sum_1d_array.py
@@ -1,9 +1,9 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        new_arr = []
+        result = []
         temp = 0
         for i in range(len(nums)):
             digit = nums[i] + temp
             temp = digit
-            new_arr.append(digit)
-        return new_arr
+            result.append(digit)
+        return result
