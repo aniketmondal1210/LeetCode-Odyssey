f3b00fbf261ebd23bf262e68b9586961107eef92
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-09-20 10:35:27 +0530

    Fix reverse value calculation in reverseDegree method

--- 3498. Reverse Degree of a String/reverse_degree_of_a_string.py
+++ 3498. Reverse Degree of a String/reverse_degree_of_a_string.py
@@ -2,6 +2,6 @@ class Solution:
     def reverseDegree(self, s: str) -> int:
         result = 0
         for i in range(1,len(s)+1):
-            reverse_value = 123 - ord(s[i-1])
+            reverse_value = 26 - (ord(s[i-1]) - ord('a'))
             result += reverse_value*i
         return result
